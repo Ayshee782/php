fafdcfe842aa2470dcac1825625d676110a6dd43
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-03-08 02:40:37 +0600

    Add files via upload

---
+++ sum of the first 10 natural numbers.php
@@ -0,0 +1,18 @@
+<?php 
+//alculate the sum of the first 10 natural numbers.
+
+$sum=0;
+for($i=0;$i<=10;$i++){
+
+    echo $i."<br><hr><b>";
+    $sum+=$i;
+
+    
+
+
+
+}
+
+
+echo "<hr>all natural numbers= ". $sum;
+?>
