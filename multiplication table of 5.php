674deb8b7ba17c61fcd8dea16ea7e387f6b888a1
Ayshee <152168245+Ayshee782@users.noreply.github.com>
2025-03-08 03:01:22 +0600

    Add files via upload

---
+++ multiplication table of 5.php
@@ -0,0 +1,19 @@
+<?php 
+//Use a for loop to display the multiplication table of 5.
+
+$mul=5;
+
+for($i=1;$i<=10;$i++)
+{
+
+    //echo $i."<br>";
+
+    echo "$mul * $i= ".($mul*$i)."<br>";
+    //$mul*=$i;
+
+
+
+}
+//echo "$mul";
+
+?>
